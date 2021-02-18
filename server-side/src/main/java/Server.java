e1ab1e0ccec798a29b7abf14f81e34ca6d8f1833
johanDDC <ivan,peshekhonov@gmail.com>
2021-02-18 20:46:27 +0300

    Add server maven module

---
+++ server-side/src/main/java/Server.java
@@ -0,0 +1,20 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+    private static final String address = "localhost";
+    private static final int port = 8080;
+
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress host = new InetSocketAddress(address, port);
+        HttpServer server = HttpServer.create(host, 10);
+        try {
+            server.start();
+            System.out.printf("Server started on %s:%d\n", address, port);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+}
