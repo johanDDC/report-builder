b4196614d21c5586ac470ecf1589ec6fff612ef9
johanDDC <ivan,peshekhonov@gmail.com>
2021-02-28 19:35:46 +0300

    Implemented simply

--- web-ui/src/main/HelloWorld.tsx
+++ web-ui/src/main/HelloWorld.tsx
@@ -2,12 +2,21 @@ import React from "react";
 
 import {withLoadedPromiseAndParams} from "./utils";
 import * as API from "./api";
+import {Messages} from "./messagesApi/messages";
+
+function codeExecuter(code: string) {
+    return new Function('messages', code);
+}
 
 function LoadedHelloWorld(props: { data: API.State }) {
+    Messages.bindBlock("root");
+    let executeFunc = codeExecuter("messages.add('Hello'); messages.add('World!');");
+    executeFunc(Messages);
+
     return <div>
-            <h1>Message: {props.data.message}</h1>
-            <h1>Time: {props.data.time.toString()}</h1>
-        </div>
+        <h1>Message: {props.data.message}</h1>
+        <h1>Time: {props.data.time.toString()}</h1>
+    </div>
 }
 
 const HelloWorld = withLoadedPromiseAndParams(API.requestState, LoadedHelloWorld, {});
