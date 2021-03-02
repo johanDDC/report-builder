2b383578227f9e7c14c73c8a1412be61b7e586f0
johanDDC <ivan,peshekhonov@gmail.com>
2021-03-02 14:37:07 +0300

    Append code extraction

--- web-ui/src/main/basic-editor.tsx
+++ web-ui/src/main/basic-editor.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
 // @ts-ignore
 import Editor from "@monaco-editor/react";
 
 function BasicEditor() {
-    const code = "function a(){}";
-    return <Editor
-        height="90vh"
-        defaultLanguage="javascript"
-        defaultValue={code}
-    />;
+    const editorRef = useRef(null);
+    const code = "function a(){\n\tconsole.log(123);\n}";
+
+    const showCode = () => {
+        alert(editorRef.current.getValue());
+    }
+
+    return <>
+        <Editor
+            height="200px"
+            defaultLanguage="typescript"
+            defaultValue={code}
+            onMount={(editor) => {
+                editorRef.current = editor;
+            }}
+        />
+        <button onClick={() => showCode()}>Print code</button>
+    </>;
 }
 
 export default BasicEditor;
