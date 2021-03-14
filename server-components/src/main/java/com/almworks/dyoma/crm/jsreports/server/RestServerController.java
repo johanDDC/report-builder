eca6fecfe89b91d57117a4ce5a6d092de8f34489
Dmitry Peshehonov <dyoma@almworks.com>
2021-03-14 14:39:26 +0300

    Sampple Singleton component

---
+++ server-components/src/main/java/com/almworks/dyoma/crm/jsreports/server/RestServerController.java
@@ -0,0 +1,31 @@
+package com.almworks.dyoma.crm.jsreports.server;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author dyoma
+ */
+@Path("server")
+@Consumes({MediaType.APPLICATION_JSON, "text/json"})
+@Produces({MediaType.APPLICATION_JSON, "text/json"})
+public class RestServerController {
+  private final DummyServerController myController;
+
+  @Inject
+  public RestServerController(DummyServerController controller) {
+    myController = controller;
+  }
+
+  @POST
+  @Path("shutdown")
+  public Response shutdown() {
+    myController.shutdown();
+    return Response.status(Response.Status.NO_CONTENT).build();
+  }
+}
--- web-ui/src/main/index.tsx
+++ web-ui/src/main/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './HelloWorld';
 import BasicExecutor from "./basicExecution";
 import BasicEditor from "./basic-editor";
+import ServerController from "./serverController";
 
 document.addEventListener('DOMContentLoaded', () => {
     let content = <>
         <HelloWorld/>
         <BasicExecutor/>
         <BasicEditor/>
+        <ServerController/>
     </>
     ReactDOM.render(content, document.querySelector('#root'));
 });
