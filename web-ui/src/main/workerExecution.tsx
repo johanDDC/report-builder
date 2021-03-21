0275d16f8da1421e80de5b50224faba83ead5bba
johanDDC <ivan,peshekhonov@gmail.com>
2021-03-21 20:39:51 +0300

    Fix

--- web-ui/src/main/workerExecution.tsx
+++ web-ui/src/main/workerExecution.tsx
@@ -1,14 +1,4 @@
-import React, {useState} from 'react';
-
-/**
- * @param worker
- * @param code      The code which will be executed
- * @param context   The object key -> value, where key is the name
- *                  of the argument (will be used in Function construction), and
- *                  value is actual argument which will be passed in constructed
- *                  Function (in other words, value is a context, in what code
- *                  will be executed).
- */
+import React from 'react';
 
 function runCode(worker: Worker, code: string, context = {}): Promise<MessageEvent> {
     worker.postMessage([code, context]);
@@ -16,24 +6,18 @@ function runCode(worker: Worker, code: string, context = {}): Promise<MessageEve
 }
 
 export default function WorkerExecuter(props: { editor: { getValue: () => string } }) {
-    const [resulExecution, setResultExecution] = useState([]);
     let worker;
     if (window.Worker) {
-        worker = new Worker("js/execution.worker.js");
+        worker = new Worker("js/worker.execution.js");
     } else {
         alert("Problem with worker initialization");
     }
 
     return <>
         <button onClick={() => {
-            runCode(worker, props.editor.getValue()).then(result => {
-                if (result !== undefined) {
-                    setResultExecution([...resulExecution, result]);
-                }
-            })
+            runCode(worker, props.editor.getValue())
         }}>
             Run in worker
         </button>
-        {resulExecution.map((val, key) => <div key={key}>{val}</div>)}
     </>
 }
