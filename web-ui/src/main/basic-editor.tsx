fcd4ff170e3d7cdc7220e0f9b939dc7e227405e8
johanDDC <ivan,peshekhonov@gmail.com>
2021-03-28 17:06:12 +0300

    Simplify table drawing; save queried data

--- web-ui/src/main/basic-editor.tsx
+++ web-ui/src/main/basic-editor.tsx
@@ -9,63 +9,43 @@ const messagesSource = [
     "}"
 ].join('\n');
 
-function drawTable(setter: React.Dispatch<React.SetStateAction<JSX.Element>>, data: Array<JSON>) {
-    /**
-     *
-     * @param element: value of JSON. May be either string, number, array or object;
-     * @param cumulativeArr: array with "primitive elements". Filling recursively, contains items which will be presented in a cell of a table;
-     * @param layout: boolean val. Indicator: is it necessary to parse the content of the array (for multidimensional arrays);
-     */
-    function addEntry(element: any, cumulativeArr: Array<string>, layout = 0) {
-        if (element instanceof Array && !(element[0] instanceof Object)) {
-            if (layout == 0) {
-                for (let el of element) {
-                    addEntry(el, cumulativeArr);
-                }
-            } else {
-                cumulativeArr.push(JSON.stringify(element));
-                return;
-            }
-        } else if (element instanceof Array && element[0] instanceof Object) {
-            for (let el of element) {
-                addEntry(el, cumulativeArr, 1);
-            }
-        } else if (element instanceof Object) {
-            cumulativeArr.push(JSON.stringify(element));
-        } else {
-            cumulativeArr.push(element);
-        }
+function Table(props: { data: Array<JSON> }) {
+    if (props.data.length == 0) {
+        return null;
     }
-
-    setter(<table>
+    return <table className="tableContainer">
         <thead>
         <tr>
             {
-                Object.keys(data[0]).map((col, key) => col != "_id" ? <th key={key}>{col}</th> : null)
+                Object.keys(props.data[0]).map((col, key) => col != "_id" ? <th key={key}>{col}</th> : null)
             }
         </tr>
         </thead>
         <tbody>
-        {data.map((obj, key) => {
+        {props.data.map((obj, key) => {
             let row = [];
             let id = 0;
             for (let field of Object.keys(obj)) {
                 if (field !== "_id") {
-                    let elements = [];
-                    addEntry(obj[field], elements);
-                    row.push(<td key={id++}>{elements.join(", ")}</td>);
+                    let elem;
+                    if (obj[field] instanceof Object) {
+                        elem = JSON.stringify(obj[field]);
+                    } else {
+                        elem = obj[field];
+                    }
+                    row.push(<td key={id++}>{elem}</td>);
                 }
             }
             return <tr key={key}>{row}</tr>
         })}
         </tbody>
-    </table>);
+    </table>;
 }
 
 
 function BasicEditor() {
     const editorContainer = useRef(null);
-    const [table, setTable] = useState(<table className="tableContainer"></table>);
+    const [data, setData] = useState([]);
     const [editor, setEditor] = useState(null);
     // const code = "function a(){\n\tconsole.log(123);\n}\n\na(); ";
     const code = "api.table([{\"_id\": {\"timestamp\": 1615836987, \"counter\": 2698868, \"randomValue1\": 4797697, \"randomValue2\": 29416}, \"show_id\": \"s1781\", \"type\": \"TV Show\", \"title\": \"Disenchantment\", \"cast\": [\"Abbi Jacobson\", \" Eric André\", \" Nat Faxon\", \" John DiMaggio\", \" Tress MacNeille\", \" Matt Berry\", \" David Herman\", \" Maurice LaMarche\", \" Lucy Montgomery\", \" Billy West\"], \"country\": \"United States\", \"date_added\": \"Jan 15, 2021, 12:00:00 AM\", \"release_year\": 2021, \"rating\": \"TV-14\", \"duration\": {\"seasons\": 3}, \"listed_in\": [\"TV Action \u0026 Adventure\", \" TV Comedies\", \" TV Sci-Fi \u0026 Fantasy\"], \"description\": \"Princess duties call, but she\u0027d rather be drinking. Free-spirited Bean exasperates the king as she wreaks havoc with her demon and elf pals.\"}])";
@@ -87,13 +67,13 @@ function BasicEditor() {
     }, []);
 
     return <>
-        <div className="basic-editor" ref={editorContainer}></div>
+        <div className="basic-editor" ref={editorContainer}/>
         <button onClick={showCode}>Print code</button>
         <button
-            onClick={() => runCode(editor.getValue()).then(message => drawTable(setTable, message.data))}>
+            onClick={() => runCode(editor.getValue()).then(message => setData(message.data))}>
             Run in worker
         </button>
-        {table}
+        <Table data={data}/>
     </>;
 }
 
