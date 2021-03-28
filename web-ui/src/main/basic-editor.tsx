59b92aa86ef32fc5ef552408105995c13615c328
johanDDC <ivan,peshekhonov@gmail.com>
2021-03-28 16:30:00 +0300

    Restore functionality

--- web-ui/src/main/basic-editor.tsx
+++ web-ui/src/main/basic-editor.tsx
@@ -65,8 +65,10 @@ function drawTable(setter: React.Dispatch<React.SetStateAction<JSX.Element>>, da
 
 function BasicEditor() {
     const editorContainer = useRef(null);
+    const [table, setTable] = useState(<table className="tableContainer"></table>);
     const [editor, setEditor] = useState(null);
-    const code = "function a(){\n\tconsole.log(123);\n}\n\na(); ";
+    // const code = "function a(){\n\tconsole.log(123);\n}\n\na(); ";
+    const code = "api.table([{\"_id\": {\"timestamp\": 1615836987, \"counter\": 2698868, \"randomValue1\": 4797697, \"randomValue2\": 29416}, \"show_id\": \"s1781\", \"type\": \"TV Show\", \"title\": \"Disenchantment\", \"cast\": [\"Abbi Jacobson\", \" Eric André\", \" Nat Faxon\", \" John DiMaggio\", \" Tress MacNeille\", \" Matt Berry\", \" David Herman\", \" Maurice LaMarche\", \" Lucy Montgomery\", \" Billy West\"], \"country\": \"United States\", \"date_added\": \"Jan 15, 2021, 12:00:00 AM\", \"release_year\": 2021, \"rating\": \"TV-14\", \"duration\": {\"seasons\": 3}, \"listed_in\": [\"TV Action \u0026 Adventure\", \" TV Comedies\", \" TV Sci-Fi \u0026 Fantasy\"], \"description\": \"Princess duties call, but she\u0027d rather be drinking. Free-spirited Bean exasperates the king as she wreaks havoc with her demon and elf pals.\"}])";
 
     const showCode = () => {
         alert(editor.getValue());
@@ -87,9 +89,11 @@ function BasicEditor() {
     return <>
         <div className="basic-editor" ref={editorContainer}></div>
         <button onClick={showCode}>Print code</button>
-        <button onClick={() => runCode(editor.getValue())}>
+        <button
+            onClick={() => runCode(editor.getValue()).then(message => drawTable(setTable, message.data))}>
             Run in worker
         </button>
+        {table}
     </>;
 }
 
