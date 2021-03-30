ed40446c587e70ad39c7fd1fcc9c50a19b5749c8
johanDDC <ivan,peshekhonov@gmail.com>
2021-03-30 12:50:43 +0300

    Fix relative URL

--- web-ui/src/main/editor/reportAPI.ts
+++ web-ui/src/main/editor/reportAPI.ts
@@ -1,6 +1,6 @@
 export class ReportAPI {
 
-    private static url = "../rest/api/v1/db/testCollection/query/";
+    private static url = new URL(location.protocol + location.host + "/rest/api/v1/db/testCollection/query/");
 
     constructor() {
         console.log("costructed");
@@ -8,6 +8,7 @@ export class ReportAPI {
 
     query(query: {}) {
         let request = new XMLHttpRequest();
+        // @ts-ignore
         request.open("POST", ReportAPI.url, false);
         request.setRequestHeader("Content-Type", "application/json");
         request.send(JSON.stringify({"query": query}));
