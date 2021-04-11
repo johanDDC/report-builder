8d3bf6da67b4d6577a67f2398795184cb509752f
johanDDC <ivan,peshekhonov@gmail.com>
2021-04-11 03:06:02 +0300

    Fix table render component with respect to table config

--- web-ui/src/main/editor/basic-editor.tsx
+++ web-ui/src/main/editor/basic-editor.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useRef, useState} from 'react';
 // @ts-ignore
 import loader from '@monaco-editor/loader';
 import runCode from "./workerExecution";
+import {EMPTY_CONFIG, TableConfig} from "./reportAPI";
 
 const messagesSource = [
     "const messages = {",
@@ -9,32 +10,35 @@ const messagesSource = [
     "}"
 ].join('\n');
 
-function Table(props: { data: Array<JSON> }) {
-    if (props.data.length == 0) {
+function Table(props: { message: { data: Array<any>, config: TableConfig } }) {
+    let columns = props.message.config.columns;
+    let rows = props.message.config.rowsToView
+    if (props.message.data.length == 0 || columns.length == 0) {
         return null;
     }
     return <table className="tableContainer">
         <thead>
         <tr>
             {
-                Object.keys(props.data[0]).map((col, key) => col != "_id" ? <th key={key}>{col}</th> : null)
+                columns.map((colName, key) => <th key={key}>{colName}</th>)
             }
         </tr>
         </thead>
         <tbody>
-        {props.data.map((obj, key) => {
+        {props.message.data.map((obj, key) => {
+            if (key > rows) {
+                return null;
+            }
             let row = [];
-            let id = 0;
-            for (let field of Object.keys(obj)) {
-                if (field !== "_id") {
-                    let elem;
-                    if (obj[field] instanceof Object) {
-                        elem = JSON.stringify(obj[field]);
-                    } else {
-                        elem = obj[field];
-                    }
-                    row.push(<td key={id++}>{elem}</td>);
+            let rowId = 0;
+            for (let field of columns) {
+                let elem;
+                if (obj[field] instanceof Object) {     // obj[field] == undefined ?
+                    elem = JSON.stringify(obj[field]);
+                } else {
+                    elem = obj[field];
                 }
+                row.push(<td key={rowId++}>{elem}</td>);
             }
             return <tr key={key}>{row}</tr>
         })}
@@ -45,9 +49,13 @@ function Table(props: { data: Array<JSON> }) {
 
 function BasicEditor() {
     const editorContainer = useRef(null);
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({data: [], config: EMPTY_CONFIG});
     const [editor, setEditor] = useState(null);
-    const code = "let result = api.query({});\n" +
+    const code = "let config = {\n" +
+        "\tcolumns: [\"show_id\", \"type\", \"title\", \"director\", \"cast\", \"country\", \"duration\", \"description\"],\n" +
+        "\trowsToView : 10,\n}\n\n" +
+        "let result = api.query({});\n" +
+        "api.configure(config);\n" +
         "api.table(result);";
 
     const showCode = () => {
@@ -73,7 +81,7 @@ function BasicEditor() {
             onClick={() => runCode(editor.getValue()).then(message => setData(message.data))}>
             Run in worker
         </button>
-        <Table data={data}/>
+        <Table message={data}/>
     </>;
 }
 
