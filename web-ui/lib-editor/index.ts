c0eb7037f0cd3d074208d3971a945d9be8ea6680
Dmitry Peshehonov <dyoma@almworks.com>
2021-04-15 22:51:33 +0300

    Fix lib-editor

--- web-ui/lib-editor/index.ts
+++ web-ui/lib-editor/index.ts
@@ -1,1 +1,2 @@
-export * from "../src/main/editor/basic-editor";
+export {BasicEditor, toCSV} from "../src/main/editor/basic-editor";
+export {WorkerManager} from "../src/main/editor/workerExecution"
