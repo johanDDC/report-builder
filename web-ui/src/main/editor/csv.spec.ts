9046d6d2df51d5d9d6ef25cb87aac4bc6c655e3d
johanDDC <ivan,peshekhonov@gmail.com>
2021-04-16 20:17:11 +0300

    Add tests

--- web-ui/src/main/editor/csv.spec.ts
+++ web-ui/src/main/editor/csv.spec.ts
@@ -3,18 +3,22 @@ import {expect} from 'chai';
 import 'mocha';
 
 describe('CSV', () => {
-  it('escape', () => {
-    expect(toCSV([1], [{header: 'H', renderer: t => '' + t}])).eq('H\r\n1\r\n');
-    expect(toCSV(['aaa', 'bbb'], [
-      {header: 'C1', renderer: t => '1' + t},
-      {header: 'C2', renderer: t => '2' + t}
-    ])).eq('C1,C2\r\n1aaa,2aaa\r\n1bbb,2bbb\r\n');
-    expect(toCSV(["aa,a", "bb, b"], [{header: 'C', renderer: t => '' + t}])).eq('C\r\naa,a\r\nbb, b\r\n');
-    expect(toCSV(["a\"a\"a", "\"bb, b\""], [{header: 'C', renderer: t => '' + t}])).eq('C\r\na"a"a\r\n"bb, b"\r\n');
-  });
-  it('spaces', () => {
-    expect(toCSV(["aa aa", "bbb b", "ccc  ", " aaa"], [
-      {header: "C 1", renderer: t => '' + t},
-    ])).eq('C 1\r\naa aa\r\nbbb b\r\nccc  \r\n aaa\r\n');
-  });
+    it('escape', () => {
+        expect(toCSV([1], [{header: 'H', renderer: t => '' + t}])).eq('H\r\n1\r\n');
+        expect(toCSV(['aaa', 'bbb'], [
+            {header: 'C1', renderer: t => '1' + t},
+            {header: 'C2', renderer: t => '2' + t}
+        ])).eq('C1,C2\r\n1aaa,2aaa\r\n1bbb,2bbb\r\n');
+        expect(toCSV(["aa,a", "bb, b"], [{header: 'C', renderer: t => '' + t}]))
+            .eq('C\r\n"aa,a"\r\n"bb, b"\r\n');
+        expect(toCSV(["a\"a\"a", "\"bb, b\""], [{header: 'C', renderer: t => '' + t}]))
+            .eq('C\r\n"a\"a\"a"\r\n"\"bb, b\""\r\n');
+        expect(toCSV(["a\r\na", "bb\r\n", "\r\ncc"], [{header: 'C', renderer: t => '' + t}]))
+            .eq('C\r\n"a\r\na"\r\n"bb\r\n"\r\n"\r\ncc"\r\n');
+    });
+    it('spaces', () => {
+        expect(toCSV(["aa aa", "bbb b", "ccc  ", " aaa"], [
+            {header: "C 1", renderer: t => '' + t},
+        ])).eq('C 1\r\naa aa\r\nbbb b\r\nccc  \r\n aaa\r\n');
+    });
 })
