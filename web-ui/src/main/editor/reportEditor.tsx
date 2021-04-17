7f4a1e03d215064198f8bac6e408da9c37b235db
Dmitry Peshehonov <dyoma@almworks.com>
2021-04-17 22:35:54 +0300

    Transpile TypeScript to JS

--- web-ui/src/main/editor/reportEditor.tsx
+++ web-ui/src/main/editor/reportEditor.tsx
@@ -1,14 +1,54 @@
 import * as React from 'react';
-import {EditorController} from "./monacoController";
+import {EditorController, Languages} from "./monacoController";
+import * as Babel from "babel__core";
+
+export namespace MIME {
+  export const TS = 'application/typescript'
+  export const JS = 'application/javascript' // https://tools.ietf.org/html/rfc4329
+}
+
+/** Defines code and its language (by MIME type) */
+export interface Code {
+  mime: string
+  text: string
+}
+
+export function transpileToJs(code: Code): string {
+  if (MIME.JS === code.mime) return code.text
+  if (MIME.TS === code.mime)
+    return Babel.transform(code.text, {
+      filename: 'example.ts', // Workaround https://github.com/babel/babel/issues/10154
+      presets: ['typescript']
+    }).code
+  throw Error("Unknown code MIME type: " + code.mime)
+}
 
 export class ReportEditorController {
-  private readonly _apiExtensions: {[key: string]: string} = {}
+  private readonly _apiExtensions: {[key: string]: Code} = {}
 
   constructor(private readonly _controller = new EditorController()) {
   }
 
   static use(): ReportEditorController {
     return React.useRef(new ReportEditorController()).current
+  }
+
+  get code(): Code {
+    let codeEditor = this.controller.editor;
+    let mime;
+    if (codeEditor) {
+      let lang = codeEditor.getModel().getModeId();
+      if (Languages.typescript === lang) mime = MIME.TS
+      else if (Languages.javascript == lang) mime = MIME.JS
+      else {
+        console.error('Unknown language: ' + lang)
+        mime = MIME.TS
+      }
+    } else {
+      console.error('Editor is not ready yet')
+      mime = MIME.TS
+    }
+    return {mime, text: this.controller.codeText}
   }
 
   get controller() { return this._controller }
@@ -20,11 +60,11 @@ export class ReportEditorController {
    * @param key the identifier of the extension to add/update/remove
    * @param types types definition to be appended to the editor
    *              May be empty/null if the extension does not provide any API (it only executes some start up)
-   * @param jsCode extension code to be prepended right before execution
+   * @param code extension code to be prepended right before execution
    *               May be empty/null if the extension does not provide any runtime (it only declares some types)
    */
-  setApiExtension(key: string, types: string, jsCode: string) {
-    if (jsCode) this._apiExtensions[key] = jsCode
+  setApiExtension(key: string, types: string, code: Code) {
+    if (code) this._apiExtensions[key] = code
     else delete this._apiExtensions[key]
     this._controller.typescriptLibs.setLibContent(key, types)
   }
@@ -38,9 +78,9 @@ export class ReportEditorController {
     Object.keys(this._apiExtensions).sort() // Sort in order to append extensions in some reproducible order
         .forEach(k => {
           if (extensions.length > 0) extensions += '\n'
-          extensions += `// Extension: ${k}\n${this._apiExtensions[k]}`
+          extensions += `// Extension: ${k}\n${transpileToJs(this._apiExtensions[k])}`
         })
-    let mainPart = this._controller.codeText;
+    let mainPart = transpileToJs(this.code);
     if (extensions.length == 0) return mainPart
     return `${extensions}\n// MAIN PART\n${mainPart}`
   }
--- web-ui/webpack.config.js
+++ web-ui/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         'react-dom': 'ReactDOM',
         'axios': 'axios',
         '@monaco-editor/loader': 'monaco_loader',
+        'babel__core': 'Babel'
     },
     module: {
         rules: [
