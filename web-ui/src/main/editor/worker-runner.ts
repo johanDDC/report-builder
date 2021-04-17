c4d8456979fa52608d1011014c2935a6085db886
johanDDC <ivan,peshekhonov@gmail.com>
2021-04-17 13:48:15 +0300

    Add optional arguments

--- web-ui/src/main/editor/worker-runner.ts
+++ web-ui/src/main/editor/worker-runner.ts
@@ -1,10 +1,24 @@
 import {processMessage} from "./worker";
-import {HttpQueryRequest, MongoQuery} from "./reportAPI"
+import {HttpQueryRequest, MongoProjection, MongoQuery} from "./reportAPI"
 
-function buildRequest(query: MongoQuery): HttpQueryRequest {
+function buildRequest(query: MongoQuery, projection?: MongoProjection,
+                      limit?: number, offset?: number, sort?: number): HttpQueryRequest {
+    let payload = {query: query.query};
+    if (projection != undefined) {
+        payload["projection"] = projection;
+    }
+    if (limit != undefined) {
+        payload["limit"] = limit;
+    }
+    if (offset != undefined) {
+        payload["offset"] = offset;
+    }
+    if (sort != undefined) {
+        payload["sort"] = sort;
+    }
     return {
         url: '/rest/api/v1/db/testCollection/query/',
-        payload: {query: query.query}
+        payload: payload,
     }
 }
 
